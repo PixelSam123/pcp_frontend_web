95be16c70031b994c76b0b8a4bc56bf7d7f5654f
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-09-13 19:01:33 +0700

    add margin and close button to dialog component

--- app/components/TheDialogPortal.tsx
+++ app/components/TheDialogPortal.tsx
@@ -1,4 +1,5 @@
 import * as Dialog from '@radix-ui/react-dialog'
+import { IconX } from '@tabler/icons-react'
 
 export default function TheDialogPortal({
   title,
@@ -13,8 +14,10 @@ export default function TheDialogPortal({
     <Dialog.Portal>
       <Dialog.Overlay className="fixed inset-0 z-10 bg-black/50" />
       <Dialog.Content
-        className="fixed left-1/2 top-1/2 z-10 max-h-screen -translate-x-1/2 -translate-y-1/2 overflow-y-auto bg-[#1e1e1e]"
+        className="fixed left-1/2 top-1/2 z-10 max-h-[calc(100vh-1.5rem)] -translate-x-1/2 -translate-y-1/2 overflow-y-auto bg-[#1e1e1e]"
       >
+        <Dialog.Close className="fixed top-3 right-3"><IconX /></Dialog.Close>
+
         <div className="the-card space-y-3">
           {title && <Dialog.Title className="font-bold">{title}</Dialog.Title>}
           {description && (
