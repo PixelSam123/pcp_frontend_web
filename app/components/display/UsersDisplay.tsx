e59f51d96c09192b0f3ed0bd1123c09137d8d1cd
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-09-16 11:59:11 +0700

    add number in leaderboard

--- app/components/display/UsersDisplay.tsx
+++ app/components/display/UsersDisplay.tsx
@@ -5,13 +5,15 @@ export default function UsersDisplay({ users }: { users: UserBriefDto[] }) {
     <table className="border-collapse border border-white">
       <thead>
         <tr>
+          <th className="border border-white px-3 py-1">#</th>
           <th className="border border-white px-3 py-1">Name</th>
           <th className="border border-white px-3 py-1">Points</th>
         </tr>
       </thead>
       <tbody>
-        {users.map((user) => (
+        {users.map((user, idx) => (
           <tr key={user.id}>
+            <td className="border border-white px-3 py-1">{idx + 1}</td>
             <td className="border border-white px-3 py-1">{user.name}</td>
             <td className="border border-white px-3 py-1">{user.points}</td>
           </tr>
